357517bbfda073e3cbeb4c0081923413d6b1cd43
Seasawher <tellagoodstory@icloud.com>
2023-07-08 13:42:18 +0000

    設定ファイルの読み込みを試す

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use clap::Parser;
-fn main() {
-    println!("Hello World!");
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct MyConfig {
+    version: u8,
+    api_key: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cfg: MyConfig = confy::load("my-app-name", None)?;
+    dbg!(cfg);
+    Ok(())
 }
